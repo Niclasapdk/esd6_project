f9b0fcc9c7ff464ccb549f697eb00a2939811d90
rasmus <rasm274@gmail.com>
2025-02-28 10:20:52 +0100

    tinglingsding

---
+++ badWahbah/Wah-Wah-master/dinlingdingbingming.m
@@ -0,0 +1,51 @@
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%   Efekt chorus
+%   Efekt chorus (brzmienia chóralnego) sygna³u dŸwiêkowego
+%   Parametry:
+%       input- sygna³ wejœciowy (podany w próbkach)
+%       Fs- czêstotliwoœæ próbkowania sygna³u oryginalnego 
+%       delay- bazowa wartoœæ opóznienia sygna³u
+%       rate_1, rate_2- czêstotliwoœæ "przemiatania" sygna³u oscylatorem 
+%       LFO (pierwszego oraz drugiego)
+%       depth_1, depth_2- amplituda sygna³u moduluj¹cego obu oscylatorów
+%       wzglêdem sygna³u wejœciowego
+%       version- wersja efektu FIR lub IIR
+%   Opis dzialania:
+%       Struktura podobna do struktury efektu flanger. Na sygna³ nak³adane
+%       s¹ dwa lekko opóŸnione i zmodulowane sygna³y, czego efektem jest 
+%       charakterystyczne "chóralne" brzmienie dŸwiêku. Czêstotliwoœæ
+%       sygna³ów moduluj¹cych powinna byæ w granicach (0.1-0.5Hz).
+%       Amplitudy tych sygna³ów wzglêdem sygna³u oryginalnego mog¹ byæ
+%       modyfikowane.
+%       
+%   Przyk³adowe wywo³anie
+%       output = chorus(sygna³_audio, 44100, 15, 0.1, 0.5, 0.2, 0.3 'FIR');  
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+[x,Fs]=audioread('guitar.wav'); 
+output = chorus(x, 44100, 15, 0.1, 0.5, 0.2, 0.3, 'FIR');
+
+
+function output = chorus(input, fs, delay, rate_1, depth_1, rate_2, depth_2, version)
+% Obliczenie liczby probek opoznienia sygnalu audio
+max_delay = floor(delay*fs/1000);
+
+% Obliczenie czêstotliwoœci unormowanych sygna³ów moduluj¹cych
+frequency_change1= rate_1/fs;
+frequency_change2= rate_2/fs;
+
+output = input(:, 1);
+n = max_delay+1:length(input);
+delay_time1 = floor(max_delay*abs(sin(2*pi*frequency_change1*n)));
+delay_time2 = floor(max_delay*abs(sin(2*pi*frequency_change2*n)));
+
+if(strcmp(version,'FIR'))
+    % Realizacja efektu Chorus w wersji SOI 
+    output(n) =1/((1+depth_1+depth_2))* (input(n, 1) + depth_1*input(n-delay_time1, 1) + depth_2*input(n-delay_time2, 1));
+    
+else
+     for n = max_delay+1:length(input)
+         % Realizacja efektu Chorus w wersji NOI 
+        output(n) = 1/((1+depth_1+depth_2))*(input(n, 1) + depth_1*output(n-delay_time1(n-max_delay)) + depth_2*output(n-delay_time2(n-max_delay)));
+     end
+end
+end
--- badWahbah/Wah-Wah-master/flanger.m
+++ badWahbah/Wah-Wah-master/flanger.m
@@ -1,5 +1,5 @@
 
-function out = flanger(in,mix,delay,width,rate,fs)
+function out = flanger(in,mix,delay,width,rate,fs,voices)
 %FLANGER simulates a guitar flanger effect pedal
 %   IN - input vector
 %   MIX - depth - amt of delayed signal added to IN (0 to 1)
@@ -14,20 +14,43 @@ fsn=fs*4;
 minDelaySamp=ceil(delay*fsn/1000); %convert to msec, then samples
 maxDelaySamp=ceil((delay+width)*fsn/1000); %convert to msec, then samples
 n=(1:length(in)+maxDelaySamp)'; %how long to extend in by for LFO
-LFO=sawtooth(2*pi*rate/(fsn)*n,.5); %sawtooth more commonly used in flangers
-delayTimeSamples=(delay+width/2+width/2*LFO)*fsn/1000; 
+%LFO=sawtooth(2*pi*rate/(fsn)*n,.5); %sawtooth more commonly used in flangers
+LFO=sin(2*pi*rate/(fsn)*n); % sin implemtation also works fine
+
+for e = 1:voices
+    delayTimeSamples(e, :) = ceil((delay + 0.1*(e-1) + width/2 + width/2 * LFO) * fsn / 1000);
+end
+%delayTimeSamples=(delay+width/2+width/2*LFO)*fsn/1000; 
 % instantaneous delay in samples (computed by looking at graph from class
 % PDF)
-
+%{
+delayTimeSamples1=(delay+width/2+width/2*LFO)*fsn/1000; 
+delayTimeSamples2=(delay/1.2+width/2+width/2*LFO)*fsn/1000; 
+delayTimeSamples3=(delay/1.1+width/2+width/2*LFO)*fsn/1000; 
+delayTimeSamples4=(delay/1.3+width/2+width/2*LFO)*fsn/1000; 
+%}
 out=zeros(length(in)+minDelaySamp,1); %initialized output vec
 
 out(1:maxDelaySamp)=in(1:maxDelaySamp); 
 % copy front of signal before min delay
+for i = maxDelaySamp + 1 : length(in)  
+    delayedSignal = 0; % Initialize mixed delayed signal
 
+    % Add all delayed voices using matrix lookup
+    for e = 1:voices
+        delaySamples = delayTimeSamples(e, i); % Get delay for this voice at sample i
+        delayedSignal = delayedSignal + mix * in(i - delaySamples);
+    end
+    out(i) = in(i) + delayedSignal;
+end
+
+
+%{
 for i=maxDelaySamp+1:length(in) % starting from next sample
     delaySamples=ceil(delayTimeSamples(i)); %whole number of current delay
     out(i)=in(i)+mix*out(i-delaySamples); %add input and fraction of delay 
 end
+%}
 
 out=downsample(out,4);
 
