28e2f5527513c278e091f238c7d77249f9f47f0d
Snorre <snorrejohnsen@gmail.com>
2025-02-28 11:36:36 +0100

    reverb stinky

--- system_design/reverb/soundtest_reverb.m
+++ system_design/reverb/soundtest_reverb.m
@@ -3,6 +3,17 @@
 %normalize sound because dododododo
 input_sound = input / max(abs(input));
 
+%try sinus
+%fs = 44100;
+%duration = 0.2;
+%f = 100;
+%t = 0:1/fs:duration;
+%input_sound = sin(2*pi*f*t);
+
+%tryimpulse
+%impulse = zeros(N,1);
+%impulse(1) = 1;
+
 %reverb parameters
 gain = 0.5;
 reverb_time = 0.1;
