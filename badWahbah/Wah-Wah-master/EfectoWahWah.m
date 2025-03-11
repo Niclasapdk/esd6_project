c2dd019a97782692b0656dec90c169b94fa8208f
rasmus <rasm274@gmail.com>
2025-03-11 15:19:00 +0100

    dingladnf

--- badWahbah/Wah-Wah-master/EfectoWahWah.m
+++ badWahbah/Wah-Wah-master/EfectoWahWah.m
@@ -5,58 +5,55 @@ function y=EfectoWahWah(damp,minf,maxf,Fs,Fw, x,Fc,wahType)
 % Fc needs to be in the range if not then end
 if Fc > maxf | Fc < minf 
         disp('No work Fc not in value !!!!!!!!!!!!!!!!!!!!!!!!!');
-        disp('You fucking dumbasss');
-        y=zeros(size(x));
         return;
 end
 
 switch wahType
-    case 1 % triangle wave / wahwah effect, making the wah wah sound
-        Fc=minf;
-    % tunning coeffiecients and start Fc
-    Q1 = 2*damp;
-    F1 = 2*sin((pi*Fc)/Fs);
-    y=zeros(size(x));
+    case 1                      % triangle wave / wahwah effect, making the wah wah sound
+        Fc=minf;                % Set cutoff to min frequency
+    % tunning coeffiecients from eq 2.8 (56 PDF DAFX)
+    Q1 = 2*damp;                %Q coefficient
+    F1 = 2*sin((pi*Fc)/Fs);     %F coefficient
+    y=zeros(size(x));           %Vector full 0's for output
     % first element in the vector
-    y(1) = F1*x(1);
-    yl = F1*y(1);
-    yaux = y(1); 
-    delta = Fw/Fs; % step size
-            for n=2:length(x),
-        if Fc + delta > maxf | Fc + delta < minf 
+    y(1) = F1*x(1);             % First output eq 2.7 (56 PDF DAFX), as NO priovious output yet
+    yl = F1*y(1);               % First Lowpass coefficient eq 2.7 (56 PDF DAFX) no priovious
+    yaux = y(1);                % First output
+    delta = Fw/Fs;              % Define step size based upon how large a frequency range it sweeps per sec
+    % Loop to create the triangle wave going from minf-maxf-minf and so on
+            for n=2:length(x),  
+        if Fc + delta > maxf | Fc + delta < minf % Change if it goes up or down when hiting maxf or minf
             delta =  -delta;
         end
-        Fc = Fc+ delta;
-        F1 = 2*sin((pi*Fc)/Fs);
-        yh = x(n)-yl-Q1*yaux; % highpass
-        y(n) = F1*yh+yaux; % bandpass
-        yl=F1*y(n)+yl; % lowpass
-        yaux=y(n); 
+        Fc = Fc+ delta;                          % increase Fc by stepsize
+        F1 = 2*sin((pi*Fc)/Fs);                  % Calculate F1 coefficient for each Fc
+        yh = x(n)-yl-Q1*yaux;                    % highpass coefficient
+        y(n) = F1*yh+yaux;                       % bandpass coefficient
+        yl=F1*y(n)+yl;                           % lowpass coeficient
+        yaux=y(n);                               % set the output = bandpass
             end
-    case 2 % for constant Fc same loop as shown above for triangle
-
-    delta = Fw/Fs; % step size
-    % vector with all possible frequencies
-    freq=minf:delta:maxf;
-    % find the closest frequency to Fc
-    [~, idx] = min(abs(freq - Fc));  % idx gives the index of the closest value
-    Fc = freq(idx);  % This is the closest frequency to Fc (finite length)
-    y=zeros(size(x));
+    case 2                           % Constant Fc
+    delta = Fw/Fs;                   % step size
+    freq=minf:delta:maxf;            % vector with all possible frequencies
+    [~, idx] = min(abs(freq - Fc));  % index of the closest value
+    Fc = freq(idx);                  % Set Fc to closest value
+    y=zeros(size(x));                % Vector full 0's for output
     % tunning coeffiecients and start Fc
-    Q1 = 2*damp;
-    F1 = 2*sin((pi*Fc)/Fs);
+    Q1 = 2*damp;                     % Q coefficient
+    F1 = 2*sin((pi*Fc)/Fs);          % F coefficient
     % first element in the vector
-    y(1) = F1*x(1);
-    yl = F1*y(1);
-    yaux = y(1); 
+    y(1) = F1*x(1);                  % First output as NO priovious output
+    yl = F1*y(1);                    % First Lowpass coefficient No priovious
+    yaux = y(1);                     % First output
+    % Loop to create the triangle wave going from minf-maxf-minf and so on
         for n=2:length(x),
-    yh = x(n)-yl-Q1*yaux; % highpass
-    y(n) = F1*yh+yaux; % bandpass
-    yl=F1*y(n)+yl; % lowpass
-    yaux=y(n); 
+    % all equations from eq 2.7 (56 PDF DAFX)
+    yh = x(n)-yl-Q1*yaux;            % highpass coefficient
+    y(n) = F1*yh+yaux;               % bandpass coefficient
+    yl=F1*y(n)+yl;                   % lowpass coefficient
+    yaux=y(n);                       % set the output = bandpass
         end
     otherwise
-            y=zeros(size(x));
     error('Invalid wahType! Must be 1 or 2.');
 end
          % needs to do both
