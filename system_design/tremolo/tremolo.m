36755c1c39016047e0a49e015648d817bc0fd36b
rasmus <rasm274@gmail.com>
2025-03-14 14:26:55 +0100

    tremolo dnfsianfd

--- system_design/tremolo/tremolo.m
+++ system_design/tremolo/tremolo.m
@@ -1,19 +1,24 @@
 %Tremolo
 
 clear; clc;
+%Parameter declaration
+[input, fs] = audioread('input.wav');   %Input and samplingfreq
+Modfreq = 5;                            %LFO frequency
+depth = 3;                              %tremolo depth
+function output=tremolo(Modfreq,depth,fs,input)
 
-[filename,pathname] = uigetfile('*.*','Select input audio file');
+%signal modulation
+t = (0:length(input)-1)/fs;                 %create index for lfo
+lfo = 1+depth*sin(2*pi*Modfreq*t);          %creates lfo with freq and depth
+%lfo = 1+depth*sawtooth(2*pi*Modfreq*t,.5)  %Triangle wave
+%lfo = 1+depth*square(2*pi*Modfreq*t)       %Square wave
+output = input .* lfo;                      %AM modulate input with lfo
 
-[x1, fs] = audioread(num2str(filename));
+% Normalize the output based of max value of in and out
+overall_gain = max(abs(input(:))) / max(abs(output(:)));
+output = output * overall_gain;
 
-t = [0:length(x1)-1]/fs;
-t = t(:);
-
-f = 5; %LFO frequency
-td = 3; %tremolo depth
-
-lfo = 1+td*sin(2*pi*f*t); %creates lfo with modification from depth
-
-out = x1 .* lfo; %AM modulation
-
-audiowrite('guitar_tremolo.wav', out/max(abs(out)), fs);
+%outputs
+sound(output, fs); % Play the processed sound
+audiowrite('output_tremolo.wav', output, fs); %save file
+end
