829e59da8d5c653ec11a8f2eacd81428c4b31c06
Mathias Joergensen <mathiasjoergensen@hotmail.com>
2025-03-27 16:20:40 +0100

    tremolo :)

--- implementation/tremolo.c
+++ implementation/tremolo.c
@@ -1,16 +1,40 @@
 /* Tremolo demolo semolo boom!!!!!*/
+#include "tremolo.h"
 
-/*Function for deciding parameter 1 modulation frequency*/
-Int16 Modfrequency(Int16 x1) {
+// Tremolo = Input * (i_val + depth * LFO) 
 
+// Rate for LFO
+Int16 rateTremSet(Int16 rate){
+    static Int16 min = 100, max = 10000, port = 1; // min = 0.1Hz, max = 10Hz       
+    parameterSet(min, max, port, rate);
+} 
+
+// Tremolo Depth
+void depthTremSet(Int16 depth){
+    static Int16 min = 3277, max = 32439, port = 2; // min = 3277 = 0.1, max = 32439 = 0.99 Q(15)
+    parameterSet(min, max, port, depth);
 }
 
-/*Function for decicing parameter 2 depth*/
-Int16 depth(Int16 x2) {
-    
+// Function for Tremolo
+Int16 tremolo(Int16 xn){
+
+static int initVal = 32767; // initialValue = 32767 = 1 Q(15)   
+//update rate & depth
+static Int16 depth = 1, rate = 1;
+depthTremSet(depth);
+
+// LFO calculation
+if (LFOIndex >= 9){                 // How many times it runs with the same Rate
+    LFOValue = lfo(Rate);           // When used the same Rate for 9 times read new Rate
+    LFOIndex = 0;
+    depthRateSet(rate);
+} else {
+    LFOIndex++;                     // Increment until reach 9 then reset
 }
 
-Int16 TremoloFx(Int16 x,Int16 f,Int16 d) {
+//Tremolo = Input * (i_val + depth * LFO) 
+xn = xn * (initVal + depth * LFOValue);
 
-	/*Skal havde styr på lfo får kunne implementer*/
+return xn;
+
 }
